1887c1de437c01471b0477053a77bcd4791af9dc
lucifer1708 <20bce091@nith.ac.in>
2022-11-27 09:14:29 +0530

    everything setuped

---
+++ scripts/run.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#xrdb merge ~/.Xresources 
+#xbacklight -set 10 &
+#xset r rate 200 50 &
+
+function run {
+ if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+#run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
+#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+#run xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+#run xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#run xrandr --output DVI-I-0 --right-of HDMI-0 --auto
+#run xrandr --output DVI-1 --right-of DVI-0 --auto
+#run xrandr --output DVI-D-1 --right-of DVI-I-1 --auto
+#run xrandr --output HDMI2 --right-of HDMI1 --auto
+#autorandr horizontal
+
+run nm-applet
+run "variety"
+run xfce4-power-manager
+run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+picom -b  --config ~/.config/arco-chadwm/picom/picom.conf &
+run "numlockx on"
+run "volumeicon"
+sxhkd -c ~/.config/arco-chadwm/sxhkd/sxhkdrc &
+#run "nitrogen --restore"
+run "conky -c $HOME/.config/arco-chadwm/conky/system-overview"
+#you can set wallpapers in themes as well
+feh --bg-scale ~/Pictures/wallhaven-k775l1_1920x1080.png
+#feh --randomize --bg-fill /home/erik/Insync/Apps/Wallhaven/*
+#feh --bg-fill ~/.config/arco-chadwm/wallpaper/chadwm.jpg &
+
+#wallpaper for other Arch based systems
+#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
+#run applications from startup
+
+#run "insync start"
+#run "spotify"
+#run "ckb-next -b"
+#run "discord"
+#run "telegram-desktop"
+
+~/.config/arco-chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
